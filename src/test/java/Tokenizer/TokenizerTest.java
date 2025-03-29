b922b4f8aa217ecc3fd410358f2ae87e6ac32957
SengD <smdieng19@gmail.com>
2025-03-28 19:36:10 -0700

    Added various tests to check for tokenizer

--- src/test/java/Tokenizer/TokenizerTest.java
+++ src/test/java/Tokenizer/TokenizerTest.java
@@ -16,6 +16,14 @@ public class TokenizerTest {
     }
 
     @Test
+    public void testVeryWrong() {
+        final Tokenizer tokenizer = new Tokenizer("@int");
+        tokenizer.skipWhitespace();
+        assertEquals(Optional.empty(),
+        tokenizer.tryReadIdentifierOrReservedWordToken());
+    }
+
+    @Test
     public void testSingleWhitespace() {
         final Tokenizer tokenizer = new Tokenizer(" ");
         tokenizer.skipWhitespace();
@@ -59,4 +67,81 @@ public class TokenizerTest {
             },
             tokens.toArray());
     }
+
+    @Test
+    public void testReservedWordsToken() throws TokenizerException {
+        final Tokenizer tokenizer = new Tokenizer("break class boolean else if extends init method new return super while void String true false");
+        final ArrayList<Token> tokens = tokenizer.tokenize();
+        assertArrayEquals(new Token[]{
+            new BreakToken(),
+            new ClassToken(), 
+            new BooleanToken(),
+            new ElseToken(), 
+            new IfToken(), 
+            new ExtendsToken(),
+            new InitToken(), 
+            new MethodToken(),
+            new NewToken(),
+            new ReturnToken(),
+            new SuperToken(), 
+            new WhileToken(),
+            new VoidToken(),
+            new StringToken(),
+            new BooleanLiteralToken(true),
+            new BooleanLiteralToken(false),
+        },
+        tokens.toArray());
+    }
+
+    @Test
+    public void testSymbol() throws TokenizerException {
+        final Tokenizer tokenizer = new Tokenizer(", / * ( ) { } . - ;");
+        final ArrayList<Token> tokens = tokenizer.tokenize();
+        assertArrayEquals(new Token[]{
+            new CommaToken(),
+            new DivToken(), 
+            new MultToken(),
+            new LParenToken(), 
+            new RParenToken(), 
+            new LBraceToken(),
+            new RBraceToken(), 
+            new PeriodToken(),
+            new MinusToken(),
+            new SemiColonToken(),
+        },
+        tokens.toArray());
+    }
+
+    @Test
+    public void testSymbols() throws TokenizerException {
+        final Tokenizer tokenizer = new Tokenizer("== <= >= !=");
+        final ArrayList<Token> tokens = tokenizer.tokenize();
+        assertArrayEquals(new Token[]{
+            new DoubleEqualToken(),
+            new LessThanEqualToken(), 
+            new GreaterThanEqualToken(),
+            new NotEqualToken(),
+        },
+        tokens.toArray());
+    }
+
+    @Test
+    public void testRandomInt() throws TokenizerException {
+        final Tokenizer tokenizer = new Tokenizer("int x = 5 + 3;");
+        final ArrayList<Token> tokens = tokenizer.tokenize();
+        assertArrayEquals(new Token[]{
+            new IntToken(),
+            new IdentifierToken("x"), 
+            new EqualsToken(),
+            new IntegerToken(5), 
+            new PlusToken(), 
+            new IntegerToken(3),
+            new SemiColonToken(), 
+        },
+        tokens.toArray());
+    }
+
+    
+
+    
 }

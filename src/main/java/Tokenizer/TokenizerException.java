2a27a10452fb31065b80efe8c5ce86170997a79b
SengD <smdieng19@gmail.com>
2025-03-28 19:35:06 -0700

    Not sure why this is an error.

--- src/main/java/Tokenizer/TokenizerException.java
+++ src/main/java/Tokenizer/TokenizerException.java
@@ -1,8 +1,5 @@
-<<<<<<< HEAD:Tokenizer/TokenizerException.java
-=======
 package Tokenizer;
 
->>>>>>> 78a8b44a85a0e428d37ad3b3ae4a3ac3a4ce812e:src/main/java/Tokenizer/TokenizerException.java
 public class TokenizerException extends Exception {
     public TokenizerException(final String message) {
         super(message);

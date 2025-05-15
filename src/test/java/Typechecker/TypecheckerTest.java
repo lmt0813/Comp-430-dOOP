52149046bda4a41d148a5c3450d440f925d81d61
SengD <smdieng19@gmail.com>
2025-05-14 21:22:30 -0700

    Added more test cases

--- src/test/java/Typechecker/TypecheckerTest.java
+++ src/test/java/Typechecker/TypecheckerTest.java
@@ -1,6 +1,7 @@
 package Typechecker;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
 import java.util.List;
@@ -9,27 +10,29 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
+import Parser.BinOpExp;
 import Parser.BooleanExp;
 import Parser.Exp;
 import Parser.IfStmt;
 import Parser.IntExp;
 import Parser.Stmt;
+import Tokenizer.PlusOp;
 
 public class TypecheckerTest {
     @Test
     public void testValidWhileLoop() throws TypeErrorException {
-        Exp guard = new TrueExp();
+        Exp guard = new BinOpExp(new IntExp(5), new LessThanOp(), new IntExp(3));
         Stmt body = new BlockStmt(List.of(
                 new VarDecStmt(new IntType(), new Variable("x"), new IntExp(5))));
         Stmt whileStmt = new WhileStmt(guard, body);
 
         Map<Variable, Type> env = new HashMap<>();
-        Typechecker.typecheck(whileStmt, env); // Should not throw
+        Typechecker.typecheck(whileStmt, env);
     }
 
     @Test
     public void testInvalidWhileGuard() {
-        Exp guard = new IntExp(5); // Not a BoolType!
+        Exp guard = new IntExp(5);
         Stmt body = new BlockStmt(List.of());
         Stmt whileStmt = new WhileStmt(guard, body);
 
@@ -49,12 +52,12 @@ public class TypecheckerTest {
         Stmt ifStmt = new IfStmt(cond, thenBranch, Optional.of(elseBranch));
 
         Map<Variable, Type> env = new HashMap<>();
-        Typechecker.typecheck(ifStmt, env); // Should not throw
+        Typechecker.typecheck(ifStmt, env);
     }
 
     @Test
     public void testInvalidIfGuard() {
-        Exp cond = new IntExp(5); // Not a boolean!
+        Exp cond = new BinOpExp(new TrueExp(), new PlusOp(), new FalseExp());
         Stmt thenBranch = new BlockStmt(List.of());
         Stmt ifStmt = new IfStmt(cond, thenBranch, Optional.empty());
 
@@ -73,6 +76,28 @@ public class TypecheckerTest {
         Stmt ifStmt = new IfStmt(cond, thenBranch, Optional.empty());
 
         Map<Variable, Type> env = new HashMap<>();
-        Typechecker.typecheck(ifStmt, env); // Should not throw
+        Typechecker.typecheck(ifStmt, env);
+    }
+
+    @Test
+    public void testLessThanIntInt() throws TypeErrorException {
+        BinOpExp e = new BinOpExp(new IntExp(5), new LessThanOp(), new IntExp(3));
+        Type result = Typechecker.typeOf(e, new HashMap<>());
+        assertTrue(result instanceof BoolType);
+    }
+
+    @Test
+    public void testAndBoolBool() throws TypeErrorException {
+        BinOpExp e = new BinOpExp(new TrueExp(), new AndOp(), new FalseExp());
+        Type result = Typechecker.typeOf(e, new HashMap<>());
+        assertTrue(result instanceof BoolType);
+    }
+
+    @Test
+    public void testInvalidPlusIntBool() {
+        BinOpExp e = new BinOpExp(new IntExp(5), new PlusOp(), new TrueExp());
+        assertThrows(TypeErrorException.class, () -> {
+            Typechecker.typeOf(e, new HashMap<>());
+        });
     }
 }
